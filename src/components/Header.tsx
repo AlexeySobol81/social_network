56e59c5c1e2c65ab5025b472292c4c12e26beadb
AlexeySobol81 <alexeysoboll@gmail.com>
2021-04-16 14:02:45 +0300

    breakdown into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src={"https://img.freepik.com/free-psd/engraved-black-logo-mockup_125540-223.jpg?size=338&ext=jpg"}/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src={'https://tlum.ru/uploads/df36f268308f13029b39c6e6ad4b6b63ada9d84100c6fb6524b3bd851f681aa6.jpeg'}/>
+            </div>
+            <div>
+                ava+discription
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
