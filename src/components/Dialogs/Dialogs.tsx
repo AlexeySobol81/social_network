9909a1d593096071a72595a759cc8177e1433588
AlexeySobol81 <alexeysoboll@gmail.com>
2021-04-19 15:19:11 +0300

    dialog page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Denis
+                </div>
+                <div className={s.dialog}>
+                    Yulya
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Cool</div>
+            </div>
         </div>
     )
 }
